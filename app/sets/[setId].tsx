16f6b5ad9b387d6a9aea828a92af1b68afdd56e2
povario <contact@povario.com>
2025-07-11 11:49:35 -0400

    Add sets page

---
+++ app/sets/[setId].tsx
@@ -0,0 +1,57 @@
+import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import type { AxiosError } from "axios";
+import { useApi } from "@/contexts/ApiProvider";
+import { useToast } from "@/contexts/ToastProvider";
+import { ActivityIndicator, Appbar } from "react-native-paper";
+import { FlashcardSet } from "@povario/potato-study.js/models";
+import MainView from "@/components/MainView";
+
+export default function SetId() {
+  const { api } = useApi();
+  const toast = useToast();
+  const [set, setSet] = useState<FlashcardSet>();
+  const [loading, setLoading] = useState(true);
+
+  const setId = Number(
+    useLocalSearchParams<{ setId: string }>().setId
+  );
+
+  useEffect(() => {
+    async function get() {
+      try {
+        const res = await api.sets.get(setId);
+        setSet(res);
+
+        setLoading(false);
+      } catch (e) {
+        const { response } = e as AxiosError<{ name?: string, message?: string }>;
+
+        toast.error(response?.data.message ?? "Unknown error");
+        console.error(response ?? e);
+      }
+    }
+
+    get();
+  }, []);
+
+  useEffect(() => {
+    if (!set) {
+      return;
+    }
+  }, [set]);
+
+  const loadingIcon = <ActivityIndicator animating />;
+
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.Content title={set?.name} />
+      </Appbar.Header>
+
+      <MainView>
+        {loading ? loadingIcon : undefined}
+      </MainView>
+    </>
+  );
+}
---
+++ app/sets/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+import { ComponentProps } from "react";
+import { useTheme } from "react-native-paper";
+
+export default function RootLayout() {
+  const theme = useTheme();
+
+  const screenOptions: ComponentProps<typeof Stack>["screenOptions"] = {
+    headerShown: false,
+    contentStyle: {
+      backgroundColor: theme.colors.background 
+    }
+  };
+
+  return <Stack screenOptions={screenOptions} />;
+}
